afd95e4e3c533a42e6830766ff10853e1f6d81a4
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2020-04-29 20:15:17 +0300

    more and more...

--- entry.js
+++ entry.js
@@ -1,4 +1,5 @@
 import * as dateFns from 'date-fns'
 import * as dateFnsTz from 'date-fns-tz'
+import * as durationFns from 'duration-fns'
 
-export {dateFns, dateFnsTz}
+export {dateFns, dateFnsTz, durationFns}
--- pack.sh
+++ pack.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -I channel:nixos-19.03 -i bash -p nodejs
+#! nix-shell -I channel:nixos-19.09 -i bash -p nodejs
 set -eu
 
 npm install date-fns
 npm install date-fns-tz
+npm install duration-fns
 npm install parcel
 
 npx parcel build entry.js --no-cache --global dateFns
